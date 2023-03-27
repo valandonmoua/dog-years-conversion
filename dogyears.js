5f5e1b4a03a22bf7617eececa0d622d7be36514c
Va Landon Moua <valandonmoua@gmail.com>
2023-03-27 13:01:11 -0500

    First Commit
    First Date: 3-27-2023

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+//My Current Age
+const myAge = 29;
+//early years value
+let earlyYears = 2;
+earlyYears = 10.5;
+//later years calculation is my current age minus two
+let laterYears = myAge - 2;
+//multiplication of later year by 4
+laterYears = laterYears * 4;
+console.log(laterYears);
+console.log(earlyYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Va";
+console.log(`My name is ${myName}. I am ${myAge} years
+old in human years which is ${myAgeInDogYears} years old in dog years.`);
---
+++ dogyears2.js
@@ -0,0 +1,8 @@
+let myAge = 37;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = laterYears + earlyYears;
+let myName = "Joy".toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years ${myAgeInDogYears} years old in dog years.`)
